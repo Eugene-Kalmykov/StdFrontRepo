979e2cb14bcdc71844f87099886314b96869dc98
Eugene <piratsilver13@gmail.com>
2021-12-21 23:12:38 +0300

    added 17th lesson

---
+++ new/19.js
@@ -0,0 +1,15 @@
+//document.cookie = "user=111";
+
+import {sum} from './script.js';
+alert(sum(1, 2));
+
+const obj = {
+    name: "erjtieut",
+    surname: "kjrjn",
+    age: 13
+}
+
+const {name, age} = obj; // ключи дожны совпадать
+//https://trello.com/b/8NYYVvGQ/weather
+//https://10minutemail.com/
+console.log(age);
---
+++ new/script.js
@@ -0,0 +1,5 @@
+const sum = (a, b) => {
+    let c = a + b;
+    return c;
+}
+export  {sum};
